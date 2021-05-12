2ef3a8f339132c2a825a9e323b799b0d0b2db6b7
root <root@DESKTOP-4RB95J6.localdomain>
2021-05-12 23:09:55 +0900

    add data save function

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+import pickle
 from fastapi import FastAPI
 
 app = FastAPI()
 
+db_save_file = './dbdump'
+
 class MocDB:
     def __init__(self):
         self.db = {}
+        self.load()
 
     def get(self, key):
         return self.db[key]
 
     def set(self, key, value):
         self.db[key] = value
+
+    def load(self):
+        with open(db_save_file, 'rb') as f:
+            self.db = pickle.load(f)
+
+    def save(self):
+        with open(db_save_file, 'wb') as f:
+            pickle.dump(self.db, f)
+
+
 
 db = MocDB()
 
@@ -21,3 +35,8 @@ def get(key):
 @app.post('/{key}/{value}')
 def set(key, value):
     db.set(key, value)
+
+
+@app.on_event("shutdown")
+def close_event():
+    db.save()
